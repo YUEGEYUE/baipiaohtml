cf3d8631b35887c4f8be977b3a4d63ecf8b711db
527301797 <527301797@qq.com>
2019-07-13 17:05:32 +0800

    更新了card.js
    更新了card.js

--- js/card.js
+++ js/card.js
@@ -1,16 +1,25 @@
 // 关浩延
+
 $(document).ready(function(){
-    $("#vcard").append("<div "+ 'class='+'"card ghy_card_size ghy_noborder"'+">"+
-    "<div "+ 'class='+'"card-body ghy_nopadding"'+">"+
-    "<div "+ 'class='+'"card img-fluid ghy_noborder"'+">"+
-    "<img "+ 'class='+'"ghy_card_size"'+'src='+'"https://static.runoob.com/images/mix/img_avatar.png"'
-    +" alt"+'='+'"Cardimage"'+">"+
-    "<div "+ 'class='+'"card-img-overlay ghy_text_overflow ghy_color"'+">"+
-    "<i "+ 'class='+'"fa fa-play-circle fa-lg"'+">"+"</i>"+"："+"123"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+
-    "<i "+ 'class='+'"fa fa-play-circle fa-lg"'+">"+"</i>"+"："+"123"+"</br>"+
-    "hhh视频hhhh标题hhh长度hhh"+
-    "</div>"+
-    "</div>"+
-    "</div>"+
-    "</div>");
+    var obj;
+    obj=JSON.parse('{ "playVolume":"100", "commentVolume":10000, "videoTitle":"白漂测试","videoPic":"https://static.runoob.com/images/mix/img_avatar.png" }');
+    $("#vcard").append(setc(obj));
+    function setc(data){
+        var c ="<div "+ 'class='+'"card ghy_card_size ghy_noborder"'+">"+
+        "<div "+ 'class='+'"card-body ghy_nopadding"'+">"+
+        "<div "+ 'class='+'"card img-fluid ghy_noborder"'+">"+
+        "<img "+ 'class='+'"ghy_card_size"'+'src='+data.videoPic
+        +" alt"+'='+'"Cardimage"'+">"+
+        "<div "+ 'class='+'"card-img-overlay ghy_text_overflow ghy_color"'+">"+
+        "<i "+ 'class='+'"fa fa-play-circle fa-lg"'+">"+"</i>"+"："+data.playVolume+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+
+        "<i "+ 'class='+'"fa fa-list-alt fa-lg"'+">"+"</i>"+"："+data.commentVolume+"</br>"+
+        data.videoTitle+
+        "</div>"+
+        "</div>"+
+        "</div>"+
+        "</div>";
+        return c;
+    }
+    
+
 });
