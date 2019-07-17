9897457261cf8cff39072c7f3508a4fa8805a2ed
yefumei <52042566+yefumei@users.noreply.github.com>
2019-07-17 17:38:24 +0800

    yfm

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
     $("#tt1").mouseenter(function () {
         $("#tt1").click();
-        
+        // console.location.href='video.html';
+        // $(".test").hide(3000);
+
     });
 });
+
