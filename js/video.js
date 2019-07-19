65e37a00c04335fd29e5d630c6d3862ac7fe0fb6
YUEGEYUE <37266516+YUEGEYUE@users.noreply.github.com>
2019-07-19 16:59:40 +0800

    Update video.js

--- js/video.js
+++ js/video.js
@@ -2,65 +2,66 @@
 $(document).ready(function () {
     const dp = new DPlayer({
         container: document.getElementById('dplayer'),
-        video:{},
-        danmaku:{
+        video: {},
+        danmaku: {
             id: 'notest',
-            api:DANMUKEURL,
+            api: DANMUKEURL,
         }
     });
-    
-    
+
+
 
     $.ajax({
         type: "post",
         url: "http://172.16.56.161:10009/audit_video_api/video_info_id.php",
-        data: {videoId:GetQueryString("vid")},
+        data: { videoId: GetQueryString("vid") },
         dataType: "json",
         success: function (response) {
             //  alert(response[0].videoUrl);
             $("#y_title").text(response[0].videoTitle);
             $("#y_summary-info").text(response[0].videoIntroduction);
             dp.switchVideo({
-                url:SERVERCOM+"/"+response[0].videoUrl,
-                pic:SERVERCOM+"/"+response[0].videoPic
-            },{
-                id: ''+response[0].videoId,
-                api:DANMUKEURL,
-            });
+                url: SERVERCOM + "/" + response[0].videoUrl,
+                pic: SERVERCOM + "/" + response[0].videoPic
+            }, {
+                    id: '' + response[0].videoId,
+                    api: DANMUKEURL,
+                });
             getcom();
         }
     });
 });
 
 
-function getcom(){
+function getcom() {
     $.ajax({
         type: "post",
-        url: SERVERCOM+"/api/v1/comments",
-        data: {videoId:GetQueryString("vid")},
+        url: SERVERCOM + "/api/v1/comments",
+        data: { videoId: GetQueryString("vid") },
         dataType: "json",
+        error: function (re) {
+            $("#lbf_comshomw").append("<span>没有评论</span>");
+        },
         success: function (response) {
-            if(response.data.length>0){
-                for(var i=0;i<response.data.length;i++)
-                    $("#lbf_comshomw").append(comui(response.data[0]));
-            }else{
-                $("#lbf_comshomw").append("<span>没有评论</span>");
-            }
-            
+
+            for (var i = 0; i < response.data.length; i++)
+                $("#lbf_comshomw").append(comui(response.data[0]));
+
+
         }
     });
 }
 
-function comui(da){
-    var t=
-    "<div class='card bg-light text-dark'>"+
-        "<div class='card-body'>"+
-            "<blockquote class='blockquote'>"+
-                "<p>"+da.comment+"</p>"+
-                "<footer class='blockquote-footer'>"+da.userId+"</footer>"+
-            "</blockquote>"+
-        "</div>"+
-    "</div>";
+function comui(da) {
+    var t =
+        "<div class='card bg-light text-dark'>" +
+        "<div class='card-body'>" +
+        "<blockquote class='blockquote'>" +
+        "<p>" + da.comment + "</p>" +
+        "<footer class='blockquote-footer'>" + da.userId + "</footer>" +
+        "</blockquote>" +
+        "</div>" +
+        "</div>";
     return t;
 }
 
