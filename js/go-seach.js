1dec4e5ceee010098f842d956fc3f2cbe31310b2
YUEGEYUE <37266516+YUEGEYUE@users.noreply.github.com>
2019-07-19 21:44:48 +0800

    s

--- js/go-seach.js
+++ js/go-seach.js
@@ -1,33 +1,41 @@
 
 function getserchdata(kw) {
-    alert(kw);
-    var da;
-    // var fo=new FormData();
-    // fo.append('videoTitle',kw);
+
     $.ajax({
         type: "post",
         url: SERVERCOM + "/api/v1/videos",
         data: { videoTitle: kw },
         dataType: "json",
         success: function (response) {
-            alert("response.data.length:"+response.data.length);
+            // alert("response.data.length:"+response.data.length);
             if (response.data.length > 0) {
                 for (var i = 0; i < response.data.length; i++) {
                     $("#lbf-sea-vcard").append(setc(response.data[i]));
-                    
                 }
             } else {
                 da = null;
+                alert("没有找到视频");
             }
 
         }
     });
-    return da;
+
 }
 
 $(document).ready(function () {
     // alert(GetQueryString("keyword"));
-    getserchdata(GetQueryString("keyword"));
+    if (GetQueryString("keyword") != "") {
+        getserchdata(GetQueryString("keyword"));
+    }
+    $("#yfm_seabut").click(function (e) {
+        if ($("#yfm_seainput").val() == "") {
+            alert("请输入搜索词");
+        } else {
+            getserchdata();
+        }
+
+
+    });
 });
 
 // yfm
