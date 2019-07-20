f6bb21e3bb13f200f788b19d6768e59823d35d6c
YUEGEYUE <37266516+YUEGEYUE@users.noreply.github.com>
2019-07-20 14:41:10 +0800

    n

--- js/notification.js
+++ js/notification.js
@@ -6,14 +6,20 @@ function getnotification() {
                 if ($.cookie('notification') == null ||
                     $.cookie('notification') != data[0].notificationId) {
                     //更新ui
+                    $("#lbf_mess").text("有新消息");
                     $.cookie('notification',data[0].notificationId,{ expires: 7, path: '/' })
                 }
                 var len;
                 data.length >= 5 ? len = 5 : len = data.length;
                 for (var i = 0; i < len; i++) {
-                    
+                    $("#popodelt").append("<li>"+data[i].notification+"</li>");
                 }
             }
         },
     );
 }
+$("#lbf_mess").popover({
+    // trigger:'hover',
+    html:true,
+    content:$("#lbf_popom").html()
+});
